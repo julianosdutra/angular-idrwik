ac2bfece51d829c90a2e9e21450b556737e0801f
julianosdutra <julianodutra@hotmail.com>
2019-09-05 15:30:46 -0300

    4 files modified, 3 added

--- src/app/product-details/product-details.component.ts
+++ src/app/product-details/product-details.component.ts
@@ -1,19 +1,22 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+
 import { products } from '../products';
+import { CartService } from '../cart.service';
 
 @Component({
   selector: 'app-product-details',
   templateUrl: './product-details.component.html',
   styleUrls: ['./product-details.component.css']
 })
+
 export class ProductDetailsComponent implements OnInit {
   product;
 
   constructor(
-    private route:ActivatedRoute
-  ) {
-    }
+    private route:ActivatedRoute,
+    private cartService: CartService
+  ) { }
 
   ngOnInit() {
     this.route.paramMap.subscribe(params => {
@@ -21,4 +24,8 @@ export class ProductDetailsComponent implements OnInit {
     });
   }
 
+  addToCart(product){
+    window.alert('O produto foi adicionado ao carrinho!');
+    this.cartService.addToCart(product);
+  }
 }
